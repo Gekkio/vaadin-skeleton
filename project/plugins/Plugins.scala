9aed1a7286ee1aa9a421dad00dbe7e46cad9915f
Joonas Javanainen <joonas.javanainen@gmail.com>
2010-10-27 00:04:09 +0300

    New: Upgraded to sbt-vaadin-plugin 0.1.2

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,5 +3,5 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val jawsyMavenReleases = "Jawsy.fi M2 releases" at "http://oss.jawsy.fi/maven2/releases"
   val jrebel = "fi.jawsy" % "sbt-jrebel-plugin" % "0.2.0"
-  val vaadin = "fi.jawsy" % "sbt-vaadin-plugin" % "0.1.0"
+  val vaadin = "fi.jawsy" % "sbt-vaadin-plugin" % "0.1.2"
 }
